65d017ee3ccb43e6b4b66ebdd49cd90354c8732a
DUSIKAI <49443424+DUSIKAI@users.noreply.github.com>
2019-05-15 20:28:58 +0800

    30. 串联所有单词的子串
    给定一个字符串 s 和一些长度相同的单词 words。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
    
    注意子串要与 words 中的单词完全匹配，中间不能有其他字符，但不需要考虑 words 中单词串联的顺序。
    
    示例 1：
    
    输入：
      s = "barfoothefoobarman",
      words = ["foo","bar"]
    输出：[0,9]
    解释：
    从索引 0 和 9 开始的子串分别是 "barfoor" 和 "foobar" 。
    输出的顺序不重要, [9,0] 也是有效答案。
    示例 2：
    
    输入：
      s = "wordgoodgoodgoodbestword",
      words = ["word","good","best","word"]
    输出：[]

---
+++ LeetCode/Substring with Concatenation of All Words.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstring = function(s, words) {
+    if (!s.length || !words.length) return [];
+    var ans = [];
+    var toFind = {};
+
+    var m = words.length;
+    var n = words[0].length;
+
+    for (var i = 0; i < m; i++) {
+        toFind[words[i]] = (toFind[words[i]] || 0) + 1;
+    }
+
+    for (i = 0; i <= s.length - m * n; i++) {
+        var found = {};
+
+        for (var j = 0; j < m; j++) {
+            var k = i + n * j;
+            var w = s.substr(k, n);
+            if (!toFind[w]) break;
+            found[w] = (found[w] || 0) + 1;
+            if (found[w] > toFind[w]) break;
+        }
+        if (j === m) ans.push(i);
+    }
+
+    return ans;
+};
